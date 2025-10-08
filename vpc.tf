79d746f14b9630021570a03218a7f16ccb265eca
akinlonudayo <akinlonudayo@yahoo.com>
2025-10-08 17:15:49 -0400

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -128,15 +128,3 @@ resource "aws_security_group" "PublicALBSG" {
   }
 }
 
-resource "aws_instance" "EC2Instance" {
-  ami                    = var.AMI_ID
-  instance_type          = var.InstanceType
-  subnet_id              = aws_subnet.subnet1.id
-  vpc_security_group_ids = [aws_security_group.PublicEC2SG.id]
-
-  tags = {
-    Name = "EC2Instance"
-  }
-}
-
-
